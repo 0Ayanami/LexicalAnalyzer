ac07c42b07d42d08d11ada38f4ed279f1f5c4d09
Ayanami <Ayanami@bupt.edu.cn>
2023-10-10 19:33:40 +0800

    修改变量名

--- main.c
+++ main.c
@@ -8,63 +8,63 @@
 
 FILE *infile, *outfile; //文件读取
 char token[MAX];   //字符数组，存放当前正在识别的单词字符串
-char ch; //字符变量，存放当前读入的字符
+char C; //字符变量，存放当前读入的字符
 int pos = 0; //字符串的位置指针
 int lines = 1; //统计行数
 int words = 0; //统计单词个数,标点和空格不计为单词
 int chars = 0; //统计字符个数
 
-char get_char()//读取字符
+char get_char()//每调用一次就从infile中读取一个字符，并把它放入变量C中
 {
-    ch = fgetc(infile);
-    if (ch == EOF)
+    C = fgetc(infile);
+    if (C == EOF)
         printf("\n 词法分析已完成,分析结果记录在 out.txt\n\n");
     chars++;
-    return ch;
+    return C;
 }
 
-void get_nbc()//每次调用时，检查ch中的字符是否为空格，若是则反复调用get_char()直至非空
+void get_nbc()//每次调用时，检查C中的字符是否为空格，若是则反复调用get_char()直至非空
 {
-    while (ch == ' ' || ch == '\t' || ch == '\b' || ch == '\n') {
-        if (ch == '\n')
+    while (C == ' ' || C == '\t' || C == '\b' || C == '\n') {
+        if (C == '\n')
             lines++;
         get_char();
     }
 }
 
-void cat(char ch) //将ch中的字符连接到token字符串后
+void cat(char C) //将C中的字符连接到token字符串后
 {
-    token[pos++] = ch;
+    token[pos++] = C;
 }
 
-int IsLetter(char ch) //判断ch中的字符是否为字母
+int letter(char C) //判断C中的字符是否为字母
 {
-    if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+    if ((C >= 'a' && C <= 'z') || (C >= 'A' && C <= 'Z'))
         return 1;
     else return 0;
 }
 
-int IsDigit(char ch) //判断ch中的字符是否为数字
+int digit(char C) //判断C中的字符是否为数字
 {
-    if (ch >= '0' && ch <= '9')
+    if (C >= '0' && C <= '9')
         return 1;
     else return 0;
 }
 
-int IsBound(char ch) //判断ch中的字符是否为分界符
+int bound(char C) //判断C中的字符是否为分界符
 {
-    if (ch == '.' || ch == '{' || ch == '}' || ch == '[' || ch == ']' || ch == '(' || ch == ')' || ch == ',' ||
-        ch == ';' ||
-        ch == '#' || ch == '\'' || ch == '\"' || ch == '\?' || ch == ':')
+    if (C == '.' || C == '{' || C == '}' || C == '[' || C == ']' || C == '(' || C == ')' || C == ',' ||
+        C == ';' ||
+        C == '#' || C == '\'' || C == '\"' || C == '\?' || C == ':')
         return 1;
     else return 0;
 }
 
-void Retract()  //向前指针回退，将ch置空
-{
-    pos--;
-    ch = ' ';
-}
+//void Retract()  //向前指针回退，将C置空
+//{
+//    pos--;
+//    C = ' ';
+//}
 
 int IsKey(char token[]) //判断token中是否为关键字
 {
@@ -120,19 +120,19 @@ int main() {
     }
     get_char();
     get_nbc();
-    while (ch != EOF) {
+    while (C != EOF) {
         //识别数字
-        if (IsDigit(ch)) {
+        if (digit(C)) {
             pos = 0;
-            while (IsDigit(ch)) {
-                cat(ch);
+            while (digit(C)) {
+                cat(C);
                 get_char();
             }
             //while循环在读到 数字开头+字母 时报错
-            if (IsLetter(ch)) {
+            if (letter(C)) {
                 token[0] = '\0';
                 fprintf(stderr, "错误：第%d行有非法字符！\n", lines); // 输出错误信息到标准错误流
-                while (IsLetter(ch))
+                while (letter(C))
                     get_char();
             }
             token[pos++] = '\0';
@@ -142,11 +142,11 @@ int main() {
             get_nbc();
         }
 
-            //识别关键字、标识符
-        else if (IsLetter(ch)) {
+        //识别关键字、标识符
+        else if (letter(C)) {
             pos = 0;
-            while (IsLetter(ch) || IsDigit(ch) || ch == '_') {
-                cat(ch);
+            while (letter(C) || digit(C) || C == '_') {
+                cat(C);
                 get_char();
             }
             token[pos++] = '\0';
@@ -159,13 +159,13 @@ int main() {
         }
 
             //识别运算符(算术运算符 关系运算符 逻辑运算符 赋值运算符)
-        else if (ch == '+') //+,++,+=
+        else if (C == '+') //+,++,+=
         {
             pos = 0;
-            cat(ch);
+            cat(C);
             get_char();
-            if (ch == '+' || ch == '=') {
-                cat(ch);
+            if (C == '+' || C == '=') {
+                cat(C);
                 token[pos++] = '\0';
                 fprintf(outfile, "%s      运算符\n", token);
                 get_char();
@@ -175,13 +175,13 @@ int main() {
                 fprintf(outfile, "%s      运算符\n", token);
                 get_nbc();
             }
-        } else if (ch == '-') //-,--,-=
+        } else if (C == '-') //-,--,-=
         {
             pos = 0;
-            cat(ch);
+            cat(C);
             get_char();
-            if (ch == '-' || ch == '=') {
-                cat(ch);
+            if (C == '-' || C == '=') {
+                cat(C);
                 token[pos++] = '\0';
                 fprintf(outfile, "%s      运算符\n", token);
                 get_char();
@@ -191,14 +191,14 @@ int main() {
                 fprintf(outfile, "%s      运算符\n", token);
                 get_nbc();
             }
-        } else if (ch == '*' || ch == '%' || ch == '!' || ch == '>' || ch == '<' ||
-                   ch == '=') //*,*=,%,%=!,!=,>,>=,<,<=,=,==
+        } else if (C == '*' || C == '%' || C == '!' || C == '>' || C == '<' ||
+                   C == '=') //*,*=,%,%=!,!=,>,>=,<,<=,=,==
         {
             pos = 0;
-            cat(ch);
+            cat(C);
             get_char();
-            if (ch == '=') {
-                cat(ch);
+            if (C == '=') {
+                cat(C);
                 token[pos++] = '\0';
                 fprintf(outfile, "%s      运算符\n", token);
                 get_char();
@@ -208,29 +208,13 @@ int main() {
                 fprintf(outfile, "%s      运算符\n", token);
                 get_nbc();
             }
-        } else if (ch == '&') // &&
+        } else if (C == '&') // &&
         {
             pos = 0;
-            cat(ch);
+            cat(C);
             get_char();
-            if (ch == '&') {
-                cat(ch);
-                token[pos++] = '\0';
-                fprintf(outfile, "%s      运算符\n", token);
-                get_char();
-                get_nbc();
-            } else {
-                fprintf(stderr, "错误：第%d行有非法字符！\n", lines); // 输出错误信息到标准错误流
-                get_char();
-                get_nbc();
-            }
-        } else if (ch == '|') // ||
-        {
-            pos = 0;
-            cat(ch);
-            get_char();
-            if (ch == '|') {
-                cat(ch);
+            if (C == '&') {
+                cat(C);
                 token[pos++] = '\0';
                 fprintf(outfile, "%s      运算符\n", token);
                 get_char();
@@ -240,43 +224,62 @@ int main() {
                 get_char();
                 get_nbc();
             }
-        } else if (ch == '/') // /,/=,//,/*
+        } else if (C == '|') // ||
         {
             pos = 0;
-            cat(ch);
+            cat(C);
             get_char();
-            if (ch == '=') {
-                cat(ch);
+            if (C == '|') {
+                cat(C);
                 token[pos++] = '\0';
                 fprintf(outfile, "%s      运算符\n", token);
                 get_char();
                 get_nbc();
-            } else if (ch == '/') // 处理//型注释
+            } else {
+                fprintf(stderr, "错误：第%d行有非法字符！\n", lines); // 输出错误信息到标准错误流
+                get_char();
+                get_nbc();
+            }
+        } else if (C == '/') // /,/=,//,/*
+        {
+            pos = 0;
+            cat(C);
+            get_char();
+            if (C == '=') {
+                cat(C);
+                token[pos++] = '\0';
+                fprintf(outfile, "%s      运算符\n", token);
+                get_char();
+                get_nbc();
+            } else if (C == '/') // 处理//型注释
             {
-                cat(ch);
+                cat(C);
                 token[pos++] = '\0';
                 fprintf(outfile, "%s      注释\n", token);
-                Retract();
-                while (ch != '\n' && ch != EOF) //可能是最后一行所以考虑EOF
+//                Retract();
+                while (C != '\n' && C != EOF) //可能是最后一行所以考虑EOF
                     get_char();
-                get_nbc();
-            } else if (ch == '*') // 处理/**/型注释
+                get_nbc(
+
+
+                        );
+            } else if (C == '*') // 处理/**/型注释
             {
-                cat(ch);
+                cat(C);
                 while (1) {
                     get_char();
-                    while (ch != '*') //一直循环直到出现*/
+                    while (C != '*') //一直循环直到出现*/
                     {
                         get_nbc(); //注释也需计算行数
                         get_char();
                     }
-                    cat(ch);
+                    cat(C);
                     get_char();
-                    if (ch == '/') {
-                        cat(ch);
+                    if (C == '/') {
+                        cat(C);
                         token[pos++] = '\0';
                         fprintf(outfile, "%s      注释\n", token);
-                        Retract();
+//                        Retract();
                         get_char();
                         get_nbc();
                         break;
@@ -291,9 +294,9 @@ int main() {
         }
 
             //识别分界符
-        else if (IsBound(ch)) {
+        else if (bound(C)) {
             pos = 0;
-            cat(ch);
+            cat(C);
             token[pos++] = '\0';
             fprintf(outfile, "%s      分界符\n", token);
             get_char();
